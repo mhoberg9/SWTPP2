869f12d575e1682981cb3678fa7ca4d75cae624a
mhoberg9 <mhoberg9@gmail.com>
2018-01-20 17:32:22 +0100

    update predicate

--- GameServerVorgabe/src/de/tuberlin/sese/swtpp/gameserver/model/Field.java
+++ GameServerVorgabe/src/de/tuberlin/sese/swtpp/gameserver/model/Field.java
@@ -6,18 +6,32 @@ import java.util.function.Predicate;
 
 public class Field extends LinkedList<Figure> {
 
+		// topLeft
+		public static BiPredicate<Figure, Figure> topLeft = (a, b) -> a.column == b.column && a.row == b.row + 1;
+		// top
+		public static BiPredicate<Figure, Figure> top = (a, b) -> a.column == b.column && a.row == b.row + 1;
+		// topRight
+		public static BiPredicate<Figure, Figure> topRight = (a, b) -> a.column == b.column + 1 && a.row == b.row + 1;
+		// left
+		public static BiPredicate<Figure, Figure> left = (a, b) -> a.column == b.column - 1 && a.row == b.row;
+		// right
+		public static BiPredicate<Figure, Figure> right = (a, b) -> a.column == b.column + 1 && a.row == b.row;
+		// bottomLeft
+		public static BiPredicate<Figure, Figure> bottomLeft = (a, b) -> a.column == b.column - 1 && a.row == b.row - 1;
+		// bottom
+		public static BiPredicate<Figure, Figure> bottom = (a, b) -> a.column == b.column && a.row == b.row - 1;
+		// bottomRight
+		public static BiPredicate<Figure, Figure> bottomRight = (a, b) -> a.column == b.column + 1 && a.row == b.row - 1;
 
-	public boolean surrounded(String moveBeginning, boolean requestingPlayer) {
-		char column = moveBeginning.charAt(0);
+		public boolean inDanger(String moveBeginning, boolean requestingPlayer) {
 
-		int row = (int) moveBeginning.charAt(1);
-		Figure f= new Figure(requestingPlayer, column, row);
+			char column = moveBeginning.charAt(0);
+			int row = (int) moveBeginning.charAt(1);
 
-		
-		
-	return this.stream().anyMatch(a->(topLeft.test(a,b)));
+			Figure f = new Figure(requestingPlayer, column, row);
 
-	
+			return this.stream().anyMatch(a -> top.test(a, f) || topRight.test(a, f) || topLeft.test(a, f)
+					|| left.test(a, f) || right.test(a, f) && !requestingPlayer);
 	}
 
 	public boolean isCannon(String unit) {
